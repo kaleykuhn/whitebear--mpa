7bd94190ea0c80078e716f784a013f82c4b6eaab
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-26 15:06:34 -0700

    add user schema and other data

---
+++ src/mock-data/users.js
@@ -0,0 +1,14 @@
+const users = [
+   {
+      id: "d4c58c04-969e-47fc-a655-82c666564645",
+      email: "kaley@gmail.com",
+      password: "5BAA61E4C9B93F3F0682250B6CF8331B7EE68FD8",
+      createdAt: 1593193401,
+   },
+   {
+      id: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
+      email: "darielle@gmail.com",
+      password: "CBFDAC6008F9CAB4083784CBD1874F76618D2A97",
+      createdAt: 1593193578,
+   },
+];
