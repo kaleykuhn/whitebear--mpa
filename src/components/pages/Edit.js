f63d0e270c5fba7a6e2a2e04caaecc1b0ffa8171
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-07-01 02:24:47 -0700

    add default value to render time data

--- src/components/pages/Edit.js
+++ src/components/pages/Edit.js
@@ -2,7 +2,8 @@ import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import Save from "../../icons/save.svg";
 import { Link } from "react-router-dom";
-
+import memoryCards from "../../mock-data/memory-cards";
+const memoryCard = memoryCards[2];
 export default function Edit() {
    return (
       <AppTemplate>
@@ -10,22 +11,31 @@ export default function Edit() {
          <div className="mb-2">
             <div className="card bg-primary">
                <div className="card-body">
-                  One morning, when Gregor Samsa woke from troubled dreams, he
-                  found himself transformed in his bed into a horrible vermin.
-                  He lay on his armour-like back, and if he lifted his head a
-                  little he could see.
-                  <textarea rows="11" className="d-sm-none"></textarea>
-                  <textarea rows="2" className="d-none d-sm-block"></textarea>
+                  <textarea
+                     rows="11"
+                     className="d-sm-none"
+                     defaultValue={memoryCard.imagery}
+                  ></textarea>
+                  <textarea
+                     rows="2"
+                     className="d-none d-sm-block"
+                     defaultValue={memoryCard.imagery}
+                  ></textarea>
                </div>
             </div>
 
             <div className="card bg-secondary">
                <div className="card-body">
-                  A wonderful serenity has taken possession of my entire soul,
-                  like these sweet mornings of spring which I enjoy with my
-                  whole heart. I am alone
-                  <textarea rows="11" className="d-sm-none"></textarea>
-                  <textarea rows="2" className="d-none d-sm-block"></textarea>
+                  <textarea
+                     rows="11"
+                     className="d-sm-none"
+                     defaultValue={memoryCard.answer}
+                  ></textarea>
+                  <textarea
+                     rows="2"
+                     className="d-none d-sm-block"
+                     defaultValue={memoryCard.answer}
+                  ></textarea>
                </div>
             </div>
          </div>
@@ -60,7 +70,7 @@ export default function Edit() {
                <h6 className="text-muted">Created on:</h6>
             </div>
             <div className="col-4 ml-6 d-flex">
-               <h6 className="">Dec. 24, 2019</h6>
+               <h6 className="">{memoryCard.createdAt}</h6>
             </div>
          </div>
          <div className="row">
@@ -68,7 +78,7 @@ export default function Edit() {
                <h6 className="text-muted">Last attempt:</h6>
             </div>
             <div className="col-4 d-flex ml-6">
-               <h6 className="">Dec. 31, 2019</h6>
+               <h6 className="">{memoryCard.lastAttemptAt}</h6>
             </div>
          </div>
          <div className="row">
@@ -76,7 +86,7 @@ export default function Edit() {
                <h6 className="text-muted">Next attempt:</h6>
             </div>
             <div className="col-4 ml-6 d-flex">
-               <h6 className="">Jul. 14, 2020</h6>
+               <h6 className="">{memoryCard.createdAt}</h6>
             </div>
          </div>
          <div className="row">
@@ -84,7 +94,7 @@ export default function Edit() {
                <h6 className="text-muted">Consecutives:</h6>
             </div>
             <div className="col-4 ml-6 d-flex">
-               <h6 className="">4</h6>
+               <h6 className="">{memoryCard.totalSuccessfulAttempts}</h6>
             </div>
          </div>
          <div className="row">
