7f117415587a6cf3fdb88211aa548b25a26fd4da
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-07-01 02:25:15 -0700

    fix mock-data

--- src/mock-data/memory-card-schema.js
+++ src/mock-data/memory-card-schema.js
@@ -1,102 +1,11 @@
-const memoryCards = [
-   {
-      id: "3b187fb9-7977-4919-b4cc-f3b2c21cdf85",
-      imagery: "A list is counted one at a time with a total number of items ",
-      answer:
-         "The reduce method executes a reducer function that you provide on each element of the array, resulting in single output value.",
-      userId: "d4c58c04-969e-47fc-a655-82c666564645",
-      createdAt: 1591873170,
-      nextAttemptAt: 1591873170,
-      lastAttemptAt: 1591725555,
-      totalSuccessfulAttempts: 0,
-      level: 1,
-   },
-   {
-      id: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
-      imagery: "Jim Thorpe has 8 octupus arms and using them to cook hash",
-      answer: 'The technical term for a hashtag is "octothorpe."',
-      userId: "d4c58c04-969e-47fc-a655-82c666564645",
-      createdAt: 1591873170,
-      nextAttemptAt: 1591873170,
-      lastAttemptAt: 1591725555,
-      totalSuccessfulAttempts: 0,
-      level: 1,
-   },
-   {
-      id: "7ddbc735-3e11-4a2b-8692-839f1ab160fc",
-      imagery:
-         "I reach in glovebox of blue Dodge Dakota to pull out a map-instead, hundreds came flying out at me all at once.",
-      answer: ".map() is asynchronous.",
-      userId: "d4c58c04-969e-47fc-a655-82c666564645",
-      createdAt: 1591873170,
-      nextAttemptAt: 1591873170,
-      lastAttemptAt: 1591725555,
-      totalSuccessfulAttempts: 0,
-      level: 1,
-   },
-
-   {
-      id: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
-      imagery: "A stick person walking from left to right leaning forward.",
-      answer:
-         "Forward slash is leaning to the right and is commonly used to describe a network a.",
-      userId: "d4c58c04-969e-47fc-a655-82c666564645",
-      createdAt: 1591873170,
-      nextAttemptAt: 1591873170,
-      lastAttemptAt: 1591725555,
-      totalSuccessfulAttempts: 0,
-      level: 1,
-   },
-
-   {
-      id: "3b187fb9-7977-4919-b4cc-f3b2c21cdf85",
-      imagery:
-         "Group of people are standing in order tallest to smallest height",
-      answer:
-         "the sort() method sorts the elements of an array in place and returns the sorted array.",
-      userId: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
-      createdAt: 1591873170,
-      nextAttemptAt: 1591873170,
-      lastAttemptAt: 1591725555,
-      totalSuccessfulAttempts: 0,
-      level: 1,
-   },
-   {
-      id: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
-      imagery:
-         "A guy I know named jason strings along a girl and  changes form into a superhero",
-      answer:
-         "The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string",
-      userId: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
-      createdAt: 1591873170,
-      nextAttemptAt: 1591873170,
-      lastAttemptAt: 1591725555,
-      totalSuccessfulAttempts: 0,
-      level: 1,
-   },
-   {
-      id: "7ddbc735-3e11-4a2b-8692-839f1ab160fc",
-      imagery: "A Dog gets some money and puts it in it's teeth",
-
-      answer:
-         "Template literals are string literals allowing embedded expressions.",
-      userId: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
-      createdAt: 1591873170,
-      nextAttemptAt: 1591873170,
-      lastAttemptAt: 1591725555,
-      totalSuccessfulAttempts: 0,
-      level: 1,
-   },
-
-   {
-      id: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
-      imagery: "A grocery list and a todo list put together into new one.",
-      answer: "The concat() method is used to merge two or more arrays",
-      userId: "13e9b9d2-9972-4e43-b3a1-c6a4887d6785",
-      createdAt: 1591873170,
-      nextAttemptAt: 1591873170,
-      lastAttemptAt: 1591725555,
-      totalSuccessfulAttempts: 0,
-      level: 1,
-   },
-];
+const memoryCardSchema = {
+   id: String,
+   imagery: String,
+   answer: String,
+   userId: String,
+   createdAt: Number,
+   nextAttemptAt: Number,
+   lasteAttemptAt: Number,
+   totalSuccessfulAttempts: Number,
+   level: Number,
+};
