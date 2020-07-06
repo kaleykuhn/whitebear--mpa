a1bab4f521282bd31b6a38568712f2c1906e13b5
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-07-06 07:41:03 -0700

    add regex helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,6 +2,7 @@ function checkIsOver(str, num) {
    if (str.length > num) return true;
    else return false;
 }
-
+// eslint-disable-next-line
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const MAX_CARD_CHARS = 240;
-export { checkIsOver, MAX_CARD_CHARS };
+export { checkIsOver, MAX_CARD_CHARS, EMAIL_REGEX };
