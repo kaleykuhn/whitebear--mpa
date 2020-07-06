4de601f3ee54200ccc2124b703faab141ddfb073
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-07-06 07:39:04 -0700

    adjust rows and col

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -6,22 +6,26 @@ export default function Landing() {
    return (
       <div className="landing-background">
          <div className="container landing">
-            <div className="row py-6">
-               <div className="col-lg-1 offset-lg-0">
+            <div className="row">
+               <div className="col-12 mt-xl-7 mt-lg-5">
                   <img
-                     className="bear logo"
+                     className="float-left mr-6 mt-2 bear logo img-fluid"
                      src={logolanding}
                      alt="white bear outline constelation shape walking left"
                   />
-               </div>
-               <div className="col-xl-7 offset-xl-1">
-                  <h1 className="bear-text">White Bear</h1>
+
+                  <h1 className="mt-2 mt-sm-0 mt-lg-2 d-inline-flex bear-text">
+                     White Bear
+                  </h1>
                </div>
             </div>
-
-            <div className="row">
-               <SignUp />
-               <Login />
+            <div className="row mt-md-6 mt-xl-8 mt-4">
+               <div className="col-xl-10 offset-xl-1 col-lg-10 offset-lg-1 col-12">
+                  <div className="row">
+                     <SignUp />
+                     <Login />
+                  </div>
+               </div>
             </div>
          </div>
       </div>
